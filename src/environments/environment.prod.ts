b5e626164cdaec2836af74e57691c705a219feb5
ElyPriscy <puyaguaric@est.ups.edu.ec>
2022-05-09 16:14:59 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   production: true,
  // base_url: 'http://localhost:3000/api'
   //base_url: 'https://tranformaciondigitalgih4pc.tech/api'
-  base_url: 'http://0.0.0.0:8080/api'
+  base_url: 'http://localhost:8080/api'
 
 };
