b90d43f3bf2bedcf4207c93688fe5b24787fe1ca
ElyPriscy <puyaguaric@est.ups.edu.ec>
2022-05-10 13:48:43 -0500

    docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
  // base_url: 'http://localhost:3000/api'
   //base_url: 'https://tranformaciondigitalgih4pc.tech/api'
-  base_url: 'http://0.0.0.0:8080/api'
+  base_url: 'https://whale-app-nq57p.ondigitalocean.app/api'
 };
