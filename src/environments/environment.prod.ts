c7e9cb465ba7824e0f6fd6ef0118778e391d9d0d
ElyPriscy <puyaguaric@est.ups.edu.ec>
2022-05-17 17:25:21 -0500

    dockerv2.8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: true,
-  // base_url: 'http://localhost:3000/api'
+  base_url: 'http://localhost:3000/api'
   //base_url: 'https://tranformaciondigitalgih4pc.tech/api'
-  base_url: 'https://lobster-app-mesmx.ondigitalocean.app/api'
+  //base_url: 'https://lobster-app-mesmx.ondigitalocean.app/api'
 };
